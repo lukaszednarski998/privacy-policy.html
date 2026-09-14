1d500c2d65a4ebf110d3cffdd0719831b5b85434
Łukasz <lukaszednarski99899@gmail.com>
2026-09-14 22:52:55 +0200

    Add wallpaper launcher activity

---
+++ hack-wallpaper/app/src/main/java/pl/arvion/hacktheworldwallpaper/MainActivity.java
@@ -0,0 +1,24 @@
+package pl.arvion.hacktheworldwallpaper;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.setWallpaperButton);
+        button.setOnClickListener(v -> openWallpaperPreview());
+    }
+
+    private void openWallpaperPreview() {
+        ComponentName component = new ComponentName(this, HackWallpaperService.class);
+        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, component);
+        startActivity(intent);
+    }
+}
